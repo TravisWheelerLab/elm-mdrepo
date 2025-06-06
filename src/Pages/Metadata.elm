621efbd910b4348738389021e86105da88c7aa72
Ken Youens-Clark <kyclark@gmail.com>
2025-06-06 11:31:05 -0700

    adding tabs to metadata

--- src/Pages/Metadata.elm
+++ src/Pages/Metadata.elm
@@ -1,11 +1,16 @@
 module Pages.Metadata exposing (Model, Msg, page)
 
+import Bootstrap.Tab as Tab
+import Bootstrap.Table as Table
 import Components.Header
 import Effect exposing (Effect)
 import Html
+import Html.Attributes exposing (class, cols, rows, value)
+import Html.Events exposing (onClick, onInput)
 import Page exposing (Page)
 import Route exposing (Route)
 import Shared
+import Task exposing (Task)
 import View exposing (View)
 
 
@@ -24,12 +29,28 @@ page shared route =
 
 
 type alias Model =
-    {}
+    { tabState : Tab.State
+    , simulation : Simulation
+    }
+
+
+defaultSimulation =
+    { shortDescription = "" }
+
+
+initialModel =
+    { tabState = Tab.initialState
+    , simulation = defaultSimulation
+    }
+
+
+type alias Simulation =
+    { shortDescription : String }
 
 
 init : () -> ( Model, Effect Msg )
 init () =
-    ( {}
+    ( initialModel
     , Effect.none
     )
 
@@ -39,14 +60,27 @@ init () =
 
 
 type Msg
-    = NoOp
+    = TabMsg Tab.State
+    | UpdateShortDesc String
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model
+        UpdateShortDesc newDesc ->
+            let
+                simulation =
+                    model.simulation
+
+                newSim =
+                    { simulation | shortDescription = newDesc }
+            in
+            ( { model | simulation = newSim }
+            , Effect.none
+            )
+
+        TabMsg state ->
+            ( { model | tabState = state }
             , Effect.none
             )
 
@@ -57,7 +91,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Tab.subscriptions model.tabState TabMsg
 
 
 
@@ -68,6 +102,85 @@ view : Shared.Model -> Model -> View Msg
 view shared model =
     Components.Header.view
         { title = "Pages.Metadata"
-        , body = [ Html.text "/metadata" ]
+        , body = viewModel shared model
         , shared = shared
         }
+
+
+viewModel : Shared.Model -> Model -> List (Html.Html Msg)
+viewModel shared model =
+    [ Html.div [ class "container" ]
+        [ Html.h1 [] [ Html.text "Make TOML" ]
+        , Tab.config TabMsg
+            |> Tab.withAnimation
+            |> Tab.center
+            |> Tab.items
+                [ Tab.item
+                    { id = "tabSimulation"
+                    , link = Tab.link [] [ Html.text "Simulation" ]
+                    , pane =
+                        Tab.pane []
+                            [ Html.br [] []
+                            , paneSimulation shared model
+                            ]
+                    }
+                , Tab.item
+                    { id = "tabToml"
+                    , link = Tab.link [] [ Html.text "TOML" ]
+                    , pane =
+                        Tab.pane []
+                            [ Html.br [] []
+                            , paneToml shared model
+                            ]
+                    }
+                ]
+            |> Tab.view model.tabState
+        ]
+    ]
+
+
+paneSimulation : Shared.Model -> Model -> Html.Html Msg
+paneSimulation shared model =
+    Table.table
+        { options = []
+        , thead = Table.simpleThead []
+        , tbody =
+            Table.tbody
+                []
+                [ Table.tr
+                    []
+                    [ Table.th [] [ Html.text "Short Desc" ]
+                    , Table.th []
+                        [ Html.input
+                            [ onInput UpdateShortDesc
+                            , value model.simulation.shortDescription
+                            ]
+                            []
+                        ]
+                    ]
+                ]
+        }
+
+
+paneToml : Shared.Model -> Model -> Html.Html Msg
+paneToml shared model =
+    let
+        sim =
+            model.simulation
+    in
+    Html.div [ class "content" ]
+        [ Html.textarea [ cols 80, rows 20 ]
+            [ Html.text <|
+                String.join "\n"
+                    [ "[initial]"
+                    , "short_description = \"" ++ sim.shortDescription ++ "\""
+                    ]
+            ]
+        ]
+
+
+
+{-
+   [ Html.div []
+   ]
+-}
